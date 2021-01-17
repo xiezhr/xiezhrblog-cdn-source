f967536b6654ea351387f660ab01b9cd7adbec1f
xiezhr <1666397814@qq.com>
2021-01-17 10:50:47 +0800

    提交博客静态资源文件

---
+++ source/js/config.js
@@ -0,0 +1,9 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+    'm786261624-7adf4da1b8397d488fb874f9', //凡尘纪
+	'm786035924-5a0066185d17e96d40f1e33d', //今生主博客
+	'm786336692-456a922340fc0827db452fe4',//张小菜
+];
+
+// refresh interval (in seconds)
+var __refresh = 120;
